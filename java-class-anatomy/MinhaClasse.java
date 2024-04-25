e5876858b2521476f060677aa9f901a5713ef20a
Carlos Vinícius Pereira <157232610+Cvinicius-7@users.noreply.github.com>
2024-04-25 02:49:01 +0000

    format document

--- java-class-anatomy/MinhaClasse.java
+++ java-class-anatomy/MinhaClasse.java
@@ -1,8 +1,8 @@
-public class MinhaClasse{
-    
-public static void main(String [] args){
-    System.out.print ("Olá Mundo !");
+public class MinhaClasse {
 
-}
+    public static void main(String[] args) {
+        System.out.print("Olá Mundo !");
+
+    }
 
 }
