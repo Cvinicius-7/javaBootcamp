77a1047d80f56fe0c8501b6bddcdfe8130ebffdc
Carlos Vinícius Pereira <157232610+Cvinicius-7@users.noreply.github.com>
2024-04-25 03:14:38 +0000

    add comments

--- java-class-anatomy/MinhaClase2.java
+++ java-class-anatomy/MinhaClase2.java
@@ -2,10 +2,9 @@ public class MinhaClase2 {
     public static void main(String[] args) 
     {
         //aula 2 de anatomia de classes java
-        final String BR = "Brasil";
-
-        int ano = 2024;
-        ano = 2025;
+       // final String BR = "Brasil";
+        //int ano = 2024;
+       // ano = 2025;
 
 
     }
