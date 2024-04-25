ea598cb86338b19b192ec62f49d3ee5727bd39e0
Carlos Vinícius Pereira <157232610+Cvinicius-7@users.noreply.github.com>
2024-04-25 02:53:47 +0000

    variable declaration study

---
+++ java-class-anatomy/MinhaClase2.java
@@ -0,0 +1,11 @@
+public class MinhaClase2 {
+    public static void main(String[] args) 
+    {
+        final String BR = "Brasil";
+        
+        int ano = 2024;
+        ano = 2025;
+
+
+    }
+}
