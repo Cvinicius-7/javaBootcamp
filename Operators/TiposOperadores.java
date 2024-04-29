17d125513316f07ce4daa5e533ec5f1c0ccf0130
Carlos Vinícius Pereira <157232610+Cvinicius-7@users.noreply.github.com>
2024-04-29 00:14:43 +0000

    Add example

---
+++ Operators/TiposOperadores.java
@@ -0,0 +1,19 @@
+public class TiposOperadores{
+    public static void main(String[] args) {
+        
+        /*EXEMPLOS:
+         * boolean doadorOrgao = false;
+         * double soma = 10.5 + 15.7;
+         * int subtracao = 113 - 25;
+         * int multiplicacao = 20 * 7;
+         * int divisao = 15 / 3;
+         * int modulo = 8 % 3;
+         * double resultado = (10 * 7) + (20 / 4);
+        */
+        
+        /*  Concatenação:
+         *  String nomeCompleto = "linguagem " + "JAVA";
+        */
+    }
+
+}
