2a3ba7a3b104e388c75d91a0973051e30f2fab08
Carlos Vinícius Pereira <157232610+Cvinicius-7@users.noreply.github.com>
2024-05-16 01:08:39 +0000

    Class example

---
+++ Operators/Relacionais.java
@@ -0,0 +1,20 @@
+public class Relacionais {
+    public static void main(String[] args) {
+        /*  Operadores de comparaçao
+         *  == igualdade
+         *  != Diferente
+         *  > Maior que
+         *  < Menor que
+         *  >= Maior ou igual
+         *  < Menor ou igual
+         */
+
+         int num1 = 1;
+         int num2 = 2;
+
+         if(num1>num2){
+         System.out.print("Numero 1 Maior");
+        }else
+         System.out.print("Numero 2 maior");
+    }
+}
