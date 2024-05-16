a59ea355233e64ae16da9b42bbe654d67735c03d
Carlos Vinícius Pereira <157232610+Cvinicius-7@users.noreply.github.com>
2024-05-16 20:49:46 +0000

    First method object implement

---
+++ Methods/SmartTV.java
@@ -0,0 +1,14 @@
+package Methods;
+
+public class SmartTV {
+    boolean ligada = false;
+    int canal = 1;
+    int volume = 25;
+
+    public void ligar(){
+        ligada = true;
+    }
+    public void desligar(){
+        ligada = false;
+    }
+}
---
+++ Methods/Usuario.java
@@ -0,0 +1,16 @@
+package Methods;
+
+public class Usuario {
+    public static void main(String[] args) {
+        
+        SmartTV smartTV = new SmartTV();
+
+        System.out.println("Tv Ligada ? " + smartTV.ligada);
+        System.out.println("Tv Canal ? " + smartTV.canal);
+        System.out.println("Tv Volume ? " + smartTV.volume);
+
+        smartTV.ligar();
+        System.out.println("*Novo Comando*" + "Tv Ligada ? " + smartTV.ligada);
+    }
+    
+}
