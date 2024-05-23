700498a653ca5ea194d553fa97025767dfa3a98e
Carlos Vinícius <p.carlosvini@gmail.com>
2024-05-22 21:53:56 -0300

    create ternary example

---
+++ Conditional-structures/ternaria.java
@@ -0,0 +1,9 @@
+public class ternaria {
+    public static void main(String[] args) {
+        double saldo = 1000.0;
+        double valorSaque = 500.0;
+
+        saldo = saldo >= valorSaque ? saldo - valorSaque : saldo;
+        System.out.println("Saldo atual: " + saldo);
+    }
+}
