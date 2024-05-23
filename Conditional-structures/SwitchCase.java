8a003a22a90ee04a82d5e07283ac44ad9f94d98d
Carlos Vinícius <p.carlosvini@gmail.com>
2024-05-22 21:59:12 -0300

    add switch case

---
+++ Conditional-structures/SwitchCase.java
@@ -0,0 +1,48 @@
+public class SwitchCase {
+    public static void main(String[] args) {
+        int mes = 13;
+        String nomeMes = "";
+        switch (mes) {
+            case 1:
+                nomeMes = "Janeiro";
+                break;
+            case 2:
+                nomeMes = "Fevereiro";
+                break;
+            case 3:
+                nomeMes = "Março";
+                break;
+            case 4:
+                nomeMes = "Abril";
+                break;
+            case 5:
+                nomeMes = "Maio";
+                break;
+            case 6:
+                nomeMes = "Junho";
+                break;
+            case 7:
+                nomeMes = "Julho";
+                break;
+            case 8:
+                nomeMes = "Agosto";
+                break;
+            case 9:
+                nomeMes = "Setembro";
+                break;
+            case 10:
+                nomeMes = "Outubro";
+                break;
+            case 11:
+                nomeMes = "Novembro";
+                break;
+            case 12:
+                nomeMes = "Dezembro";
+                break;
+            default:
+                nomeMes = "Mês inválido";
+                break;
+        }
+        System.out.println("O mês correspondente ao número " + mes + " é " + nomeMes);
+    }
+}
