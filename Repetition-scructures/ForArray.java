fcbe1371ac183e7af277a789439729548445138c
Carlos Vinícius Pereira <157232610+Cvinicius-7@users.noreply.github.com>
2024-05-28 23:00:41 +0000

    Add class example

---
+++ Repetition-scructures/ForArray.java
@@ -0,0 +1,8 @@
+public class ForArray {
+    public static void main(String[] args) {
+        String[] carneirinhos = {"Dolly", "Molly", "Polly", "Bolly", "Jolly", "Folly", "Golly", "Holly", "Lolly", "Zolly"};
+        for(int i = 0; i < carneirinhos.length; i++) {
+            System.out.println("Carneirinho " + carneirinhos[i] + " pulou a cerca");
+        }
+    }
+}
