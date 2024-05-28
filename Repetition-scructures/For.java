2de89cdb34b3566a8f0cfd590e1eb0aeb07fc4b3
Carlos Vinícius Pereira <157232610+Cvinicius-7@users.noreply.github.com>
2024-05-28 22:59:40 +0000

    add class example

---
+++ Repetition-scructures/For.java
@@ -0,0 +1,8 @@
+public class For {
+    public static void main(String[] args) {
+        for(int carneirinhos = 1; carneirinhos <= 10; carneirinhos++) {
+            System.out.println("Carneirinho " + carneirinhos + " pulou a cerca");
+        }
+    }
+    
+}
